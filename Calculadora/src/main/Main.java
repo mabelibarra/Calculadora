57d40192040fa2e81782186860206c4e72c8d2c9
ader148 <ader148@hotmail.com>
2014-11-27 12:50:07 -0500

    primer commit desde eclipse

---
+++ Calculadora/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("primer commit desde eclipse");
+
+	}
+
+}
