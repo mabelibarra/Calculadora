38d8b36036cc14604bb239862bef70e66b279734
ycradrengifo <darcy_0213@hotmail.es>
2014-11-27 18:01:47 -0500

    Update Main.java

--- Calculadora/src/main/Main.java
+++ Calculadora/src/main/Main.java
@@ -11,7 +11,51 @@ public class Main {
 		
 		System.out.println("primer commit desde eclipse");
 		
-		
+	import javax.swing.*;
+
+ 
+
+
+
+        int op=0;
+
+        double n1,n2,s,m,d,r;
+
+        do{
+
+            op=Integer.parseInt(JOptionPane.showInputDialog("nCalculadoran"+
+
+            "************n"+
+
+            "[1] SUMARn"+
+
+            "[2] RESTARn"+
+
+            "[3] MULTIPLICARn"+
+
+            "[4] DIVIDIRn"+
+
+            "[5] SALIRn"+
+
+            "Ingresa una opcion:"));
+
+ 
+
+            switch(op)
+
+            {
+
+                case 1:
+
+                    n1=Double.parseDouble(JOptionPane.showInputDialog("Ingrese numero 1"));
+
+                    n2=Double.parseDouble(JOptionPane.showInputDialog("Ingrese numero 2"));
+
+                    s=n1+n2;
+
+                    JOptionPane.showMessageDialog(null,"La suma es:"+s);
+
+                    break;	
 		
 		
 
