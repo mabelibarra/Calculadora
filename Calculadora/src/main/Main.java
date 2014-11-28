54ad0ca2c64906dc335cf8e4f0329950e022f6df
jhonathanrn <yonatanrn@unicauca.edu.co>
2014-11-28 12:35:50 -0500

    primera parte del código jhonathan
    después de esto darcy agrega su parte.

--- Calculadora/src/main/Main.java
+++ Calculadora/src/main/Main.java
@@ -1,4 +1,5 @@
 package main;
+import javax.swing.*;
 
 public class Main {
 
@@ -9,9 +10,32 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("primer commit desde eclipse");
-		
-	import javax.swing.*;
+	int op=0;
+        double n1,n2,s,m,d,r,ra,p=1,l,f=1;
+        do{
+            op=Integer.parseInt(JOptionPane.showInputDialog("\nCalculadora\n"+
+            "************************\n"+
+            
+            "[1] RESTAR\n"+
+            "[2] SUMAR\n"+
+            "[4] DIVIDIR\n"+
+            "[3] MULTIPLICAR\n"+
+            "[5] RADICACION\n"+
+            "[6] POTENCIA\n"+
+            "[7] LOGARITMO\n"+
+            "[8] FACTORIAL\n"+
+            "[9] SALIR\n"+
+            "Ingresa una opcion:\n\n"));
+
+            switch(op)
+            {
+            	case 1:
+                    n1=Double.parseDouble(JOptionPane.showInputDialog("digite numero 1"));
+                    n2=Double.parseDouble(JOptionPane.showInputDialog("digite numero 2"));
+                    s=n1+n2;
+                    JOptionPane.showMessageDialog(null,"resta:"+s);
+                    break;
+
 
  
 
